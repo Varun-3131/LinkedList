a0e449ec5e9f9ca797a17efecb3de88c2156c877
Varun-3131 <srivastavvarun48@gmail.com>
2024-02-17 21:41:32 +0530

    Linked List...

---
+++ src/main/java/main/LinkedList1.java
@@ -0,0 +1,65 @@
+package main;
+
+public class LinkedList1 {
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //To add element at head of the list...
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;                       //To get the size of the list...
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    //To add element in the middle of the list...
+    public void addMiddle(int index, int data) {
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i < index - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    //To add elements at the tail of the list
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    //To print a list...
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(STR."\{temp.data}->");
+            temp = temp.next;
+        }
+        System.out.println(" Null");
+    }
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList1 ll=new LinkedList1(); //Making of object...
+        ll.addFirst(5);              //Calling of methods...
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.print();
+        System.out.println(ll.size);
+    }
+}
