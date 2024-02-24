d162dcdb8c2361a92e0b3c37b46b636dc03ab722
Varun-3131 <srivastavvarun48@gmail.com>
2024-02-25 00:02:07 +0530

    Linked List...

--- src/main/java/main/LinkedList1.java
+++ src/main/java/main/LinkedList1.java
@@ -132,6 +132,43 @@ public class LinkedList1 {
         System.out.println(" Null");
     }
 
+    //Slow Fast Approach...
+    public Node findMid() {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next; //+1 steps
+            fast = fast.next.next;  //+2 steps
+        }
+        return slow;
+    }
+
+    public boolean CheckPalindrome() {
+        if (head == null || head.next != null) {
+            return true;
+        }
+        Node midNode = findMid();
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
     public static class Node {
         int data;
         Node next;
